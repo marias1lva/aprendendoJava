d466bd57589134d46d13bb3cb40307b1f9b4dd13
Maria Lima <mariaesilva2308@outlook.com>
2025-03-03 15:07:56 -0300

    Exercicio intervalos

---
+++ intervalos/src/Main.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+import java.util.Locale;
+
+public class Main {
+    public static void main(String[] args) {
+        Locale.setDefault(Locale.US);
+        Scanner sc = new Scanner(System.in);
+
+        double x;
+        System.out.println("Digite um valor: ");
+        x = sc.nextDouble();
+        if(x >= 0 && x <= 25.00){
+            System.out.println("Intervalo [0,25]");
+        }else if(x > 25.00 && x <= 50.00){
+            System.out.println("Intervalo (25,50]");
+        }else if(x > 50.00 && x <= 75.00){
+            System.out.println("Intervalo (50,75]");
+        }else if(x > 75.00 && x <= 100.00){
+            System.out.println("Intervalo (75,100]");
+        }else{
+            System.out.println("Fora de intervalo");
+        }
+
+        sc.close();
+    }
+}
