37c05c13d5c8802e1d2c0169ddecc9994d6e4389
Maria Lima <mariaesilva2308@outlook.com>
2025-03-03 14:44:50 -0300

    Exercicio numero negativo ou nao

---
+++ negativoOuNao/src/Main.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int x;
+        System.out.print("Digite um valor inteiro: ");
+        x = sc.nextInt();
+
+        if(x >= 0){
+            System.out.println("NAO NEGATIVO");
+        }else{
+            System.out.println("NEGATIVO");
+        }
+
+        sc.close();
+    }
+}
