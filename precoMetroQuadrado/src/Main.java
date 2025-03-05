aa4e26a44218efafd1073d04ea755a399f82cb41
Maria Lima <mariaesilva2308@outlook.com>
2025-03-05 08:53:55 -0300

    exercicio

---
+++ precoMetroQuadrado/src/Main.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+import java.util.Locale;
+
+public class Main {
+    public static void main(String[] args) {
+        Locale.setDefault(Locale.US);
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Digite a largura e o comprimento do quadrado: ");
+        double largura = sc.nextDouble();
+        double comprimento = sc.nextDouble();
+
+        System.out.print("Digite o metro quadrado: ");
+        double metroQuadrado = sc.nextDouble();
+
+        double area = largura * comprimento;
+        double preco = area * metroQuadrado;
+
+        System.out.printf("AREA = %.2f%n", area);
+        System.out.printf("PRECO = %.2f%n", preco);
+
+        sc.close();
+    }
+}
