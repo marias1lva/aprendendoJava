a4faf0f7a329ea8333945a05304ba5308e91937f
Maria Lima <mariaesilva2308@outlook.com>
2025-03-05 13:20:12 -0300

    Exercicio aplicando For

---
+++ quadradoECubo/src/Main.java
@@ -0,0 +1,19 @@
+// Fazer um programa para ler um número inteiro positivo N. O programa deve então mostrar na tela N linhas, começando de 1 até N. Para cada linha, mostrar o número
+// da linha, depois o quadrado e o cubo do valor.
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int N;
+        System.out.print("Digite um numero inteiro positivo: ");
+        N = sc.nextInt();
+        for (int i = 1; i <= N; i++) {
+            System.out.println(i + " " + i*i + " " + i*i*i);
+        }
+
+        sc.close();
+    }
+}
